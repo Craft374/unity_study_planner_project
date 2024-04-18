c64fda189cbded306fe973ccca2736d567848aab
Craf374 <leejeongwoo1103@gmail.com>
2024-04-18 14:24:56 +0900

    edit

--- study_project/Assets/script/buttontest.cs
+++ study_project/Assets/script/buttontest.cs
@@ -15,7 +15,7 @@ public class buttontest : MonoBehaviour
     public Button btn;
     void Start()
     {
-        GameObject btnobj = GameObject.Find("qwer");
+        GameObject btnobj = GameObject.Find("addbtn");
         btn = btnobj.GetComponent<Button>();
         btn.onClick.AddListener(btest);
         Debug.Log("asd");
@@ -24,12 +24,18 @@ public class buttontest : MonoBehaviour
     public void btest()
     {
         Debug.Log("asdasdasd");
-        originalImage = GameObject.Find("Image");
+        originalImage = GameObject.Find("base");
         clonedImage = Instantiate(originalImage);
         // Content 오브젝트를 찾아옵니다.
         GameObject content = GameObject.Find("Content");
 
         // 복제된 이미지를 Content의 자식으로 만듭니다.
         clonedImage.transform.SetParent(content.transform, false);
+
+        // 복제된 이미지의 이름을 설정합니다.
+        int cloneCount = content.transform.childCount - 2; // 이미 생성된 복제 오브젝트의 개수
+        clonedImage.name = "base" + cloneCount;
+        // clonedImage의 현재 순서를 base 다음으로 조절합니다.
+        clonedImage.transform.SetSiblingIndex(originalImage.transform.GetSiblingIndex() + 1);
     }
 }
